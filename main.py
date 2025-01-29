05c05b2fe3b213556b8b294915538651b7b94514
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-29 18:23:03 +0300

    add game logic code and some printing line to show the result

--- main.py
+++ main.py
@@ -10,25 +10,52 @@ else:
 
 game_elements = ["rock", "paper", "scissors"]
 symbol = [other.rock, other. paper, other.scissors]
+game_on = True
+while game_on:
+    while True:
+        user_choice = input("Enter your choice (Rock (R) 🧱, Paper (P) 📄, Scissors (S) ✂): ").lower()
+        if user_choice == "r":
+            user_choice = "rock"
+            break
+        elif user_choice == "p":
+            user_choice = "paper"
+            break
+        elif user_choice == "s":
+            user_choice = "scissors"
+            break
+        else:
+            print("Invalid input, Try again")
+    computer_choice = random.choice(game_elements)
 
-while True:
-    user_choice = input("Enter your choice (Rock (R) 🧱, Paper (P) 📄, Scissors (S) ✂): ").lower()
-    if user_choice == "r":
-        user_choice = "rock"
-        break
-    elif user_choice == "p":
-        user_choice = "paper"
-        break
-    elif user_choice == "s":
-        user_choice = "scissors"
-        break
+    # Get user and computer choices index in game elements:
+    user_index = game_elements.index(user_choice)
+    computer_index = game_elements.index(computer_choice)
+
+    # Turn User and computer choices to Symbols:
+    user_symbol = symbol[user_index]
+    computer_symbol = symbol[computer_index]
+
+    # Game Logic:
+    #Draw
+    if user_choice == computer_choice:
+        print(f"\nYour choose: \n{user_symbol} \nComputer choose: \n{computer_symbol}")
+        print(f"It's a Draw")
+
+    # User Win:
+    elif ( (user_choice == "rock" and computer_choice == "scissors") or
+           (user_choice == "paper" and computer_choice == "rock")    or
+           (user_choice == "scissors" and computer_choice == "paper") ):
+
+        print(f"\nYour choose: \n{user_symbol} \nComputer choose: \n{computer_symbol}")
+        print(f"You Win, {user_choice} beat {computer_choice}")
+
+    # User Lost:
     else:
-        print("Invalid input, Try again")
-computer_choice = random.choice(game_elements)
+        print(f"\nYour choose: \n{user_symbol} \nComputer choose: \n{computer_symbol}")
+        print(f"You Lose, {computer_choice} beat {user_choice}")
 
-user_index = game_elements.index(user_choice)
-computer_index = game_elements.index(computer_choice)
-
-
-
-
+    if input("Do you want to play again (Y/N): ").lower() != "y":
+        print("Bye Bye...")
+        game_on = False
+    else:
+        pass
